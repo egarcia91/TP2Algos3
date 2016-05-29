6e2635eac88ac4f95ec5cceeb20354e8be86cb3b
garcia ezequiel <garciaezequiel91@gmail.com>
2016-05-29 14:06:09 -0300

    Creando clase de Jugador

---
+++ src/fiuba/algo3/algoformers/Jugador.java
@@ -0,0 +1,29 @@
+package fiuba.algo3.algoformers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fiuba.algo3.algoformers.AlgoFormer;
+
+public class Jugador {
+
+	private String nombre;
+	private List<AlgoFormer> escuadron = new ArrayList<AlgoFormer>();
+
+	public void setNombre(String unNombre){
+		this.nombre = unNombre;
+	}
+
+	public String getNombre(){
+		return this.nombre;
+	}
+
+	public void asignarEscuadron(List<AlgoFormer> unEscuadron){
+		this.escuadron.addAll(unEscuadron);
+	}
+
+	public int cantidadAlgoFormer(){
+		return escuadron.size();
+	}
+
+}

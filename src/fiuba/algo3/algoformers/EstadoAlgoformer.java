c244a60931d0c11c2d3fdf4786ee6d1a3a630cb3
FedericoMuñozMacia <fede_munoz@hotmail.com>
2016-06-06 14:02:07 -0300

    Subo las clases que faltaban

---
+++ src/fiuba/algo3/algoformers/EstadoAlgoformer.java
@@ -0,0 +1,40 @@
+package fiuba.algo3.algoformers;
+
+public class EstadoAlgoformer{
+	private int vida;
+	private int velocidad;
+	private int fuerzaAtaque;
+	private int distanciaAtaque;
+	
+	public int getVida(){
+		return vida;
+	}
+	
+	public int getVelocidad(){
+		return velocidad;
+	}
+	
+	public int getFuerzaAtaque(){
+		return fuerzaAtaque;
+	}
+	
+	public int getDistanciaAtaque(){
+		return distanciaAtaque;
+	}
+	
+	public void setVida(int vida){
+		this.vida = vida;
+	}
+	
+	public void setVelocidad(int velocidad){
+		this.vida = velocidad;
+	}
+	
+	public void setFuerzaAtaque(int fuerzaAtaque){
+		this.vida = fuerzaAtaque;
+	}
+	
+	public void setDistanciaAtaque(int distanciaAtaque){
+		this.vida = distanciaAtaque;
+	}	
+}

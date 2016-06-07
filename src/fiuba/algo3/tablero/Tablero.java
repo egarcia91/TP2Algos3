1aaacc968c404a520235985ba10add753726198e
FedericoMuñozMacia <fede_munoz@hotmail.com>
2016-06-07 13:37:57 -0300

    Arreglado el merge en tablero

---
+++ src/fiuba/algo3/tablero/Tablero.java
@@ -0,0 +1,283 @@
+package fiuba.algo3.tablero;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import fiuba.algo3.algoformers.Escuadron;
+import fiuba.algo3.algoformers.Spark;
+import fiuba.algo3.algoformers.AlgoFormer;
+
+public class Tablero {
+	
+	private int ancho;
+	private int alto;
+	private Casillero[][] tablero;
+	private Map<String,Posicion> posicionesElementos;
+
+	private Escuadron escuadronUno;
+	private Escuadron escuadronDos;
+
+	public Tablero(int ancho, int alto){
+		if(ancho <= 0 || alto <= 0){
+			return;
+		}
+		tablero = new Casillero[ancho][alto];
+		for(int i = 0; i < ancho; i++){
+			for(int j = 0; j < alto; j++){
+				tablero[i][j] = new Casillero();
+			}
+		}
+		this.ancho = ancho;
+		this.alto = alto;
+		posicionesElementos = new LinkedHashMap<String,Posicion>();
+
+		escuadronUno = new Escuadron();
+		escuadronDos = new Escuadron();
+
+	}
+	
+	public int getAncho(){
+		return this.ancho;
+	}
+
+	public int getAlto(){
+		return this.alto;
+	}	
+	
+	public int getCantidadCasilleros(){
+		return ancho*alto;
+	}
+	
+	public boolean estaDesierto(){
+		return posicionesElementos.isEmpty();
+	}
+
+	public Casillero getCasillero(int posX, int posY){
+		try{
+			return tablero[posX][posY];
+		}catch(Exception e){
+			throw new CasilleroNoExisteException();
+		}
+	}		
+	
+	public Casillero getCasillero(Posicion posicion){
+		return this.getCasillero(posicion.getX(),posicion.getY());
+	}
+
+
+	public int cantidadAlgoFormer(){
+		return this.escuadronUno.cantidadMiembrosEscuadron()+ this.escuadronDos.cantidadMiembrosEscuadron();
+	}
+
+
+	
+	public void agregarAlgoFormer(AlgoFormer unAlgoFormer,int posX,int posY){
+		this.tablero[posX][posY].agregarAlgoFormer(unAlgoFormer);
+		posicionesElementos.put(unAlgoFormer.getNombre(), new Posicion(posX, posY));
+	}
+	
+	public void quitarAlgoFormer(AlgoFormer unAlgoFormer,int posX, int posY){
+		posicionesElementos.remove(unAlgoFormer.getNombre());
+		this.tablero[posX][posY].quitarAlgoFormer();
+	}
+	
+	public boolean tieneAlgoFormer(int posX,int posY){
+		return(this.tablero[posX][posY].contieneAlgoFormer());
+	}
+
+	public void setItem(Spark item,int posX, int posY){
+		tablero[posX][posY].setItem(item);		
+	}
+		
+	private Posicion buscarAlgoFormer(AlgoFormer unAlgoFormer){
+		if(posicionesElementos.containsKey(unAlgoFormer.getNombre())== true){
+			return posicionesElementos.get(unAlgoFormer.getNombre());
+
+		}
+		else
+			throw new AlgoFormerNoExisteException();
+	}
+
+	public boolean existeAlgoFormer(AlgoFormer unAlgoFormer, int posX, int posY){
+		AlgoFormer algoFormer;
+		try{
+			algoFormer = this.getCasillero(posX, posY).getAlgoFormer();
+		}
+		catch(AlgoFormerNoExisteException e){
+			return false;
+		}
+		return (algoFormer.getNombre() == unAlgoFormer.getNombre());
+	}
+	
+	public void moverIzquierda(AlgoFormer unAlgoFormer, int valor){
+		try{
+			Posicion posicion = buscarAlgoFormer(unAlgoFormer);
+			this.getCasillero(posicion).quitarAlgoFormer();
+	
+			//Ubicar nueva posicion dentro de los limites del tablero:
+			if(posicion.getX() > valor){
+				posicion.setX(posicion.getX() - valor);
+			}
+			else{
+				posicion.setX(0);
+			}
+			this.getCasillero(posicion).agregarAlgoFormer(unAlgoFormer);
+		}
+		catch(AlgoFormerNoExisteException e){
+			return;
+		}	
+	}
+
+
+
+	public void moverDerecha(AlgoFormer unAlgoFormer, int valor){
+		try{
+			Posicion posicion = buscarAlgoFormer(unAlgoFormer);
+			this.getCasillero(posicion).quitarAlgoFormer();
+	
+			//Ubicar nueva posicion dentro de los limites del tablero:
+			if(posicion.getX() < (ancho - valor)){
+				posicion.setX(posicion.getX() + valor);
+			}
+			else{
+				posicion.setX(ancho - 1);
+			}
+			this.getCasillero(posicion).agregarAlgoFormer(unAlgoFormer);
+		}
+		catch(AlgoFormerNoExisteException e){
+			return;
+		}	
+	}	
+		
+		
+	public void moverArriba(AlgoFormer unAlgoFormer, int valor){
+		try{
+			Posicion posicion = buscarAlgoFormer(unAlgoFormer);
+			this.getCasillero(posicion).quitarAlgoFormer();
+	
+			//Ubicar nueva posicion dentro de los limites del tablero:
+			if(posicion.getY() > valor){
+				posicion.setY(posicion.getY() - valor);
+			}
+			else{
+				posicion.setY(0);
+			}
+			this.getCasillero(posicion).agregarAlgoFormer(unAlgoFormer);
+		}
+		catch(AlgoFormerNoExisteException e){
+			return;
+		}	
+	}	
+	
+	public void moverAbajo(AlgoFormer unAlgoFormer, int valor){
+		try{
+			Posicion posicion = buscarAlgoFormer(unAlgoFormer);
+			this.getCasillero(posicion).quitarAlgoFormer();
+	
+			//Ubicar nueva posicion dentro de los limites del tablero:
+			if(posicion.getY() < (alto - valor)){
+				posicion.setY(posicion.getY() + valor);
+			}
+			else{
+				posicion.setY(alto - 1);
+			}
+			this.getCasillero(posicion).agregarAlgoFormer(unAlgoFormer);
+		}
+		catch(AlgoFormerNoExisteException e){
+			return;
+		}	
+	}	
+
+
+	public void ataqueZona(AlgoFormer unAlgoFormer,int unaDistanciaAtaque, int unaFuerzaAtaque){
+		Posicion posicion = this.buscarAlgoFormer(unAlgoFormer);
+		int anchoFinal = posicion.getX()+unaDistanciaAtaque;
+		int anchoInicial = posicion.getX()-unaDistanciaAtaque;
+		int altoFinal = posicion.getY()+unaDistanciaAtaque;
+		int altoInicial = posicion.getY()-unaDistanciaAtaque;
+		if (this.perteneceEscuadronUno(unAlgoFormer)) {
+			for (AlgoFormer eachAlgoFormer :
+					this.escuadronDos.algoFormers) {
+				Posicion posicionEnemigo = this.buscarAlgoFormer(eachAlgoFormer);
+				if (posicionEnemigo.getX() >= anchoInicial && posicionEnemigo.getX() <= anchoFinal && posicionEnemigo.getY() >= altoInicial && posicionEnemigo.getY() <= altoFinal) {
+					eachAlgoFormer.recibirAtaque(unaFuerzaAtaque);
+				}
+			}
+		}else{
+				for (AlgoFormer eachAlgoFormer:
+						this.escuadronUno.algoFormers){
+					Posicion posicionEnemigo = this.buscarAlgoFormer(eachAlgoFormer);
+					if (posicionEnemigo.getX() >= anchoInicial && posicionEnemigo.getX() <= anchoFinal && posicionEnemigo.getY() >= altoInicial && posicionEnemigo.getY() <= altoFinal) {
+						eachAlgoFormer.recibirAtaque(unaFuerzaAtaque);
+					}
+				}
+		}
+
+	}
+
+	private boolean perteneceEscuadronUno(AlgoFormer unAlgoFormer){
+		return this.escuadronUno.perteneceAlgoformer(unAlgoFormer);
+	}
+
+	private boolean perteneceEscuadronDos(AlgoFormer unAlgoFormer){
+		return this.escuadronDos.perteneceAlgoformer(unAlgoFormer);
+	}
+
+
+	private void ubicarEscuadronUno(){ //FIX: NO TIENE SENTIDO, INITX ES SIEMPRE 1
+		int initY = 1;
+		int initX = 1;
+		for (AlgoFormer algoFormer: this.escuadronUno.algoFormers) {
+			//algoFormer.tablero = this;
+			this.getCasillero(initX,initY).agregarAlgoFormer(algoFormer);
+			if(initX == 1){
+				initX = 2;
+				initY = 1;
+			} else {
+				initX = 1;
+				initY = 2;
+			}
+		}
+	}
+
+	private void ubicarEscuadronDos(){
+		int initY = this.alto;
+		int initX = this.ancho;
+		for (AlgoFormer algoFormer: this.escuadronDos.algoFormers){
+			//algoFormer.tablero = this;
+			this.getCasillero(initX,initY).agregarAlgoFormer(algoFormer);
+			if(initX == this.ancho){
+				initX--;
+			} else {
+				initX = this.ancho;
+				initY--;
+			}
+		}
+	}
+
+	public void agregarEscuadron(Escuadron unEscuadron) {
+		if (!escuadronUno.existeEscuadron()) {
+			this.escuadronUno.algoFormers.addAll(unEscuadron.algoFormers);
+			this.ubicarEscuadronUno();
+		} else if (!escuadronDos.existeEscuadron()) {
+			this.escuadronDos.algoFormers.addAll(unEscuadron.algoFormers);
+			this.ubicarEscuadronDos();
+		}
+	}
+
+	public void moverAlgoFormer(AlgoFormer unAlgoFormer,int deltaX, int deltaY){
+		if(!posicionesElementos.containsKey(unAlgoFormer.getNombre()))
+			throw new AlgoFormerNoExisteException();
+
+		Posicion unaPosicion = posicionesElementos.get(unAlgoFormer.getNombre());
+		int posX = unaPosicion.getX();
+		int posY = unaPosicion.getY();
+
+		if(this.tieneAlgoFormer(posX+deltaX,posY+deltaY))
+			throw new CasilleroOcupadoException();
+		else {
+				this.quitarAlgoFormer(unAlgoFormer,posX+deltaX,posY+deltaY);
+				this.agregarAlgoFormer(unAlgoFormer,posX+deltaX,posY+deltaY);
+		}
+	}
+}
+

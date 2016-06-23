de9cb9c9232bc1a6d5f5d96029d482d0781c3308
FedericoMuñozMacia <fede_munoz@hotmail.com>
2016-06-23 06:06:41 -0300

    prueba1

---
+++ src/fiuba/algo3/juegomvc/BButtonHandler.java
@@ -0,0 +1,19 @@
+package fiuba.algo3.juegomvc;
+
+import fiuba.algo3.tablero.Juego;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class BButtonHandler implements EventHandler<ActionEvent> {
+
+	public BButtonHandler(BoxView boxView, Juego juego) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void handle(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
